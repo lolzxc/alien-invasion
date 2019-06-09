ac0711f832730dcee4e6fcf1a8b5493a79b34f28
lolzxc <guitarbeginner2@gmail.com>
2019-06-09 17:06:12 +0800

    Add files via upload

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,25 @@
+import sys
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+	# Initialize pygame, settings, and screen object.
+	pygame.init()
+	ai_settings = Settings()
+	screen = pygame.display.set_mode(
+	(ai_settings.screen_width, ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invasion")
+
+	# Make a ship.
+	ship = Ship(screen)
+	
+	# Start the main loop of the game.
+	while True:
+
+		gf.check_events()
+		gf.update_screen(ai_settings, screen, ship)
+		
+run_game()
